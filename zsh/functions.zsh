2055a226ae76367095594730a8381f1c36cd7c66
Jose Lopez <jose.lopez@sendgrid.com>
2014-07-28 19:22:03 -0700

    add folder fix permissions script

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -6,3 +6,11 @@ jcurl() {
   echo 'curl -v -X$VERB -H -d '$3' -H "Content-Type: application/json" $2 | python -mjson.tool'
 }
 export jcurl
+
+# fix folder permissions: 755 folder 664 files
+fperms() {
+  chmod -R 644 $1
+  find $1 -type d -print0 | xargs -0 -II chmod 755 'I'
+}
+
+export fperms
